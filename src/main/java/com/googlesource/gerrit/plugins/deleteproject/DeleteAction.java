b2db9da48d2df068a4a3d2a53464087f0b6ea74d
Build Jenkins <build.jenkins@wandisco.com>
2024-05-08 15:24:13 +0100

    Gerrit with WANdisco replication - v2.13.12-RP-1.9.8.2
    New Features
    ------------
    - Adding WANdisco replication support.
    - Added reno release notes with config.

--- src/main/java/com/googlesource/gerrit/plugins/deleteproject/DeleteAction.java
+++ src/main/java/com/googlesource/gerrit/plugins/deleteproject/DeleteAction.java
@@ -1,3 +1,16 @@
+
+/********************************************************************************
+ * Copyright (c) 2014-2018 WANdisco
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Apache License, Version 2.0
+ *
+ ********************************************************************************/
+
 // Copyright (C) 2013 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,10 +27,12 @@
 
 package com.googlesource.gerrit.plugins.deleteproject;
 
+import com.google.gerrit.common.replication.coordinators.ReplicatedEventsCoordinator;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.extensions.webui.UiAction;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.config.AllProjectsNameProvider;
+import com.google.gerrit.server.config.AllUsersNameProvider;
 import com.google.gerrit.server.config.PluginConfigFactory;
 import com.google.gerrit.server.project.ProjectResource;
 import com.google.inject.Inject;
@@ -28,37 +43,53 @@ import com.googlesource.gerrit.plugins.deleteproject.database.DatabaseDeleteHand
 import com.googlesource.gerrit.plugins.deleteproject.fs.FilesystemDeleteHandler;
 import com.googlesource.gerrit.plugins.deleteproject.projectconfig.ProjectConfigDeleteHandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class DeleteAction extends DeleteProject implements
     UiAction<ProjectResource> {
   @Inject
   DeleteAction(AllProjectsNameProvider allProjectsNameProvider,
-      DatabaseDeleteHandler dbHandler,
-      FilesystemDeleteHandler fsHandler,
-      CacheDeleteHandler cacheHandler,
-      ProjectConfigDeleteHandler pcHandler,
-      Provider<CurrentUser> userProvider,
-      @PluginName String pluginName,
-      DeleteLog deleteLog,
-      PluginConfigFactory cfgFactory,
-      HideProject hideProject) {
-    super(allProjectsNameProvider, dbHandler, fsHandler, cacheHandler,
-        pcHandler, userProvider, pluginName, deleteLog, cfgFactory, hideProject);
+               AllUsersNameProvider allUsersNameProvider,
+               DatabaseDeleteHandler dbHandler,
+               FilesystemDeleteHandler fsHandler,
+               CacheDeleteHandler cacheHandler,
+               ProjectConfigDeleteHandler pcHandler,
+               Provider<CurrentUser> userProvider,
+               @PluginName String pluginName,
+               DeleteLog deleteLog,
+               PluginConfigFactory cfgFactory,
+               HideProject hideProject,
+               Provider<ReplicatedEventsCoordinator> replicatedEventsCoordinator) {
+    super(allProjectsNameProvider, allUsersNameProvider, dbHandler, fsHandler, cacheHandler,
+        pcHandler, userProvider, pluginName, deleteLog, cfgFactory, hideProject, replicatedEventsCoordinator);
   }
 
   @Override
   public UiAction.Description getDescription(ProjectResource rsrc) {
+    boolean replicated = true;
+    Logger log = LoggerFactory.getLogger(DeleteAction.class);
+
+    try {
+      replicated = DeleteProject.isRepoReplicated(rsrc.getName());
+    } catch (IOException e) {
+      log.error("Error accessing the project :", rsrc.getName(), " Error: ", e, " [ Assuming a replicated repo is in place. ]" );
+    }
     return new UiAction.Description()
-        .setLabel("Delete...")
-        .setTitle(isAllProjects(rsrc)
-            ? String.format("No deletion of %s project",
-                allProjectsName)
-            : String.format("Delete project %s", rsrc.getName()))
-        .setEnabled(!isAllProjects(rsrc))
+        .setLabel(replicated ? "Clean up..." : "Delete...")
+        .setTitle(isAllNamedProject(rsrc) ?
+            String.format("No deletion of %s or %s project", allProjectsName, allUsersName) :
+              String.format("%s%sproject %s",
+                  (replicated? "Clean up":"Delete"), (replicated? " replicated ":" "),
+                  rsrc.getName()))
+        .setEnabled(!isAllNamedProject(rsrc))
         .setVisible(canDelete(rsrc));
   }
 
-  private boolean isAllProjects(ProjectResource rsrc) {
-    return (rsrc.getControl().getProject()
-        .getNameKey().equals(allProjectsName));
+  private boolean isAllNamedProject(ProjectResource rsrc) {
+    String currentProject = rsrc.getControl().getProject().getName();
+    return (currentProject.equals(allProjectsName)|| currentProject.equals(allUsersName));
   }
 }
